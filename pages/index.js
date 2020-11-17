2d037e94ad8e81d3f8cf22468a4974fbaf5677b7
jad111 <jad111@duke.edu>
2020-11-16 20:25:58 -0500

    Added resume download button

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 // pages/index.js
+import "./index.scss";
+
 import BigText from "../components/BigText";
 
 import Layout from "../components/Layout";
@@ -11,6 +13,9 @@ const Index = () => (
       <BigText>
         Hi, I'm Jake. I write code for good. <SocialIcons />
       </BigText>
+      <a href="resume.pdf" download>
+        <button className="ResumeButton">Download Resume</button>
+      </a>
     </LandingPage>
   </Layout>
 );
